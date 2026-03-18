f9a223935afe2602bc2c2de068177d4bc2b84ec4
Diego Sequeira <107523673+DieS06@users.noreply.github.com>
2026-03-17 22:11:41 -0600

    FIX DUP

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,10 +20,6 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
 }
 
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
-}
-
 dependencyResolutionManagement {
     repositories {
         google()
