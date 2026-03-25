081b08456a6d8a283af8db8e315c481d8b780f5a
Diego Sequeira <107523673+DieS06@users.noreply.github.com>
2026-03-25 09:01:19 -0600

    fix(composeApp): add missing mutableStateOf import in App.kt

--- composeApp/src/commonMain/kotlin/crocalert/app/App.kt
+++ composeApp/src/commonMain/kotlin/crocalert/app/App.kt
@@ -2,6 +2,7 @@ package crocalert.app
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.navigation.compose.NavHost
